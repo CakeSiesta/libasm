7152a99f0e27ada7c5e93c92398d9cb14b1d8de0
Caketus <maxim1404@gmail.com>
2021-01-10 18:46:05 +0100

    why segv ?? :(

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -7,13 +7,13 @@ ft_strcpy:
 	xor	rax, rax
 
 cpy_loop:
-	mov	dl, BYTE [rsi + rax] ; moves src (rsi) at byte index (rax) into dl (scratch register) 
-	mov	BYTE [rdi + rax], dl ; moves byte with index (dl) to rdi (dest) at index rax
-	cmp	dl, 0
+	mov	cl, BYTE [rsi + rax] ; moves src (rsi) at byte index (rax) into cl (8bits long srcratch register) 
+	mov	BYTE [rdi + rax], cl ; moves byte with index (cl) to rdi (dest) at index rax
+	cmp	cl, 0
 	je	exit
 	inc	rax
 	jmp	cpy_loop
 
 exit:
-	mov	rax, rdi ; put value of dst (rsi) into rax (the return value register)
+	movsx	rax, cl ; put value of dst (rsi) into rax (the return value register) in correct signed register form
 	ret
