edd1127e35efc9eaa579b0867f3d7f1f58ab6728
Maxim Kravetz <mkravetz@fu-r3-p8.s19.be>
2021-05-05 16:46:01 +0200

    all mac comptible, strcpy fixed

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -1,9 +1,9 @@
 ; char *strcpy (char *dest, const char *src);
 
 section .text
-	global ft_strcpy
+	global _ft_strcpy
 
-ft_strcpy:
+_ft_strcpy:
 	xor	rcx, rcx
 	xor	rdx, rdx
 	cmp	rsi, 0
@@ -11,9 +11,9 @@ ft_strcpy:
 	jmp	cpy_loop
 
 cpy_loop:
-	mov	cl, BYTE[rcx + rsi]
-	mov	BYTE[rcx + rdi], cl
-	cmp	cl, 0
+	mov	dl, BYTE[rcx + rsi]
+	mov	BYTE[rcx + rdi], dl
+	cmp	dl, 0
 	jz	exit
 	inc	rcx
 	jmp	cpy_loop
