eba0915cbb4a3d12106eecdc7c5d6817464f6047
Caketus <maxim1404@gmail.com>
2021-05-09 22:41:28 +0200

    all non_bonus ok

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -4,23 +4,28 @@ section .text
 	global ft_strcmp
 
 ft_strcmp:
-	mov	rax, -1
+	xor	rcx, rcx
+	xor	rdx, rdx
 
 cmp_loop:
-
 	inc	rax
-	mov	cl, BYTE[rdi + rax]
-	mov	dl, BYTE[rsi + rax]
-	cmp	cl, 0 			; if str1[i] == '\0'
-	je	exit
-	cmp	dl, 0 			; if str2[i] == '\0'
-	je	exit
-	cmp	cl, dl 			; if str1[i] == str2[i]
-	je	cmp_loop
-	jmp	exit			; if str1[i] != str2[i]
+	mov	dl, BYTE[rdi + rcx]	; smaller register of rdx
+	cmp	dl, BYTE[rsi + rcx]	; compare if both strings are the same
+	jne	check_overflow
 
-exit:
-	movsx	rax, cl
-	movsx	rdx, dl
-	sub	rax, rdx		; ret the diff of str1[i] and str2[i]
+
+	cmp	dl, 0 			; check if end of both strings
+	jz	check_overflow
+	inc	rcx
+	jmp	cmp_loop
+
+check_overflow:
+	sub	dl, BYTE[rsi + rcx]
+	movzx	rax,dl
+	jnc	return			; if overflow flag inactive
+	neg	al
+	neg	eax
+	ret
+
+return:
 	ret
